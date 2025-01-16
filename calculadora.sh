ce71408e6f41df55cbfd0b61f1c497dd294524dc
Joao16007 <jptricolor72@gmail.com>
2025-01-16 12:35:21 -0300

    projeto_calculadora.py

---
+++ calculadora.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Verificação de atualização de pacotes e instalação do python.
+
+sudo apt update
+sudo apt install python3
+
+# Esse script tem por finalidade a execução de uma calculadora básica.
+
+python3 /home/joao-p/modulo1/projeto_modulo_1.py
+
